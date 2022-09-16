f8daba10ec77adcfcb177bea07443ccacfa85c77
chromatices <pjh403@naver.com>
2022-09-16 13:05:53 +0900

    change temp json

--- dev/src/main.py
+++ dev/src/main.py
@@ -5,16 +5,20 @@ from urllib import request
 # # URL
 url = os.environ['REST_URL']
 
-# # headers
-headers = os.environ['API_HEADER']
-
 # sample data
-temp = {
-    "color": "black",
-    "size": 200
+temp = { 
+    "team_id": "convai",
+    "secret": "3dlZhXRPPyt22tR9", 
+    "answer_sheet": {
+        "no": "1", 
+        "answer": "20"
+    }
 }
 
+# post to API server
 data = json.dumps(temp).encode('utf8')
-req =  request.Request(url, data=data, headers=headers)
+req =  request.Request(url, data=data)
+
+# check API server return
 resp = request.urlopen(req)
 print(resp.read().decode('utf8'))
