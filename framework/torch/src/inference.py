9e36e3d1cbcaad131b6d1a7d4e8b0375e41b9be7
chromatices <pjh403@naver.com>
2022-09-20 21:58:41 +0900

    edited that API request per batch

--- framework/torch/src/inference.py
+++ framework/torch/src/inference.py
@@ -1,19 +1,40 @@
+import json
 import torch
 from tqdm import tqdm
+from urllib import request
 
-
-def inference(model, inference_data, device):
+def inference(model, inference_data, device, url):
+    # set inference mode
     model.eval()
+    
+    # attach device to model ['cpu','cuda']
     model.to(device)
+    
     with torch.no_grad():
-        answer = []
-        for img in tqdm(inference_data):
+        # set progressbar with tqdm & return batch:int, img:torch.tensor by enumerate
+        for batch, img in tqdm(enumerate(inference_data)):
+            # attach device to tensor
             img=img.to(device)
+            
+            # get inference result
             output = model(img)
+            
+            ## set sigmoid to final layer output
             output = torch.sigmoid(output)
             probability, argmax = torch.max(output, 1)
-            pred = argmax.item()
-            answer.append(pred)
-
-    return answer
+            batch_label = argmax.tolist()
+            batch_answer = {'answer' : batch_label}
             
+            # # apply unicode to str json data
+            data = json.dumps(batch_answer).encode('unicode-escape')
+            
+            # # request ready
+            req =  request.Request(url, data=data)
+            
+            # # POST to API server
+            resp = request.urlopen(req)
+            
+            # # check POST result
+            status = resp.read().decode('utf8')
+            if "OK" in status:
+                print("batch : "+str(batch+1)+"'s result requests successful!!")
--- framework/torch/src/main.py
+++ framework/torch/src/main.py
@@ -1,38 +1,36 @@
 import os
-import json
 import torch
-from urllib import request
-
+from model import linear_model
+from inference import inference
 from torchvision import transforms
 from dataloader import MNIST_inference
-from inference import inference
-from model import linear_model
+
+
 
 def main():
     # load environment variable
-    url = os.environ['REST_URL']
+    api_url = os.environ['REST_URL']
     data_path = '/home/agc2022/data'
     
     # inference data loader define
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize(( 0.5), (0.5))])
-    inference_loader = MNIST_inference(data_dir = data_path, transform = transform)
+    
+    # load inference set & inference loader
+    inference_set = MNIST_inference(data_dir = data_path, transform = transform)
+    inference_loader =torch.utils.data.DataLoader(inference_set, batch_size=10,shuffle=False,num_workers=1)
 
+    # define device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("device : "+ str(device))
+    
     # model weight load
     model = linear_model()
-    model.load_state_dict(torch.load('./my_model/mnist_net.pth'))
+    model.load_state_dict(torch.load('./my_model/mnist_net.pth', map_location=device))
 
     # model result
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    answer=inference(model=model, inference_data= inference_loader,device=device)
-    answer_dict = {'answer' : answer}
-    data = json.dumps(answer_dict).encode('utf8')
-    
-    # request result to API
-    req =  request.Request(url, data=data)
-    resp = request.urlopen(req)
-    print(resp.read().decode('utf8'))
+    inference(model=model, inference_data= inference_loader,device=device,url=api_url)
 
 if __name__ == "__main__":
     main()
